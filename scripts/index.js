28d8b3c7225eafefae5c1127057ea3197281348c
vojdel <vasquezpinedaj@gmail.com>
2021-01-08 17:39:13 -0400

    feat: ✨ init configure of the create projects

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,72 @@
-console.log("Hello!!!");
+const inquirer = require("inquirer");
+const { Command } = require("commander");
+const program = new Command();
+const fs = require("fs");
+
+program.version("version 0.0.1");
+
+program
+  .option("-c, --create <name>", "create project")
+  .command("create <name>")
+  .action((dir) => {
+    console.log("Creating Project %s", dir);
+    fs.mkdir(`${dir}`, { recursive: true }, (err) => {
+      if (err) throw err;
+      inquirer
+        .prompt([
+          {
+            type: "rawlist",
+            name: "typeProject",
+            message: "What type of project?",
+            choices: [
+              "just Nodejs",
+              "Vue/Nodejs",
+              "Angular/Nodejs",
+              "React/Nodejs",
+            ],
+          },
+        ])
+        .then((answer) => {
+          if (answer.typeProject === "just Nodejs") {
+            fs.mkdir(`${dir}/app`, { recursive: true }, (err) => {
+              if (err) throw err;
+              fs.writeFile(`${dir}/app/index.html`, "", (err) => {
+                if (err) throw err;
+              });
+            });
+            fs.mkdir(`${dir}/assets`, { recursive: true }, (err) => {
+              if (err) throw err;
+              fs.mkdir(`${dir}/assets/js`, { recursive: true }, (err) => {
+                if (err) throw err;
+              });
+              fs.mkdir(`${dir}/assets/css`, { recursive: true }, (err) => {
+                if (err) throw err;
+              });
+            });
+            fs.mkdir(`${dir}/public`, { recursive: true }, (err) => {
+              if (err) throw err;
+            });
+            fs.mkdir(`${dir}/src`, { recursive: true }, (err) => {
+              if (err) throw err;
+              fs.writeFile(`${dir}/src/server.js`, "", (err) => {
+                if (err) throw err;
+              });
+            });
+            fs.mkdir(`${dir}/test`, { recursive: true }, (err) => {
+              if (err) throw err;
+              fs.writeFile(`${dir}/test/test.js`, "", (err) => {
+                if (err) throw err;
+              });
+            });
+            fs.mkdir(`${dir}/views`, { recursive: true }, (err) => {
+              if (err) throw err;
+            });
+            console.log("Ready projects");
+          } else {
+            console.log("in building...");
+          }
+        });
+    });
+  });
+
+program.parse(process.argv);
